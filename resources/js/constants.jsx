f1a4b371767be2662d3c3154dd369a452a73e514
Glaupex <glauberyuri160497@gmail.com>
2024-05-09 11:59:26 -0300

    feat: add constants.jsx

--- resources/js/constants.jsx
+++ resources/js/constants.jsx
@@ -1,18 +1,51 @@
 export const ITCON_PAG_STATUS_PAGAMENTO = {
     'Q' : 'Quitado',
-    'V' : 'Vencido',
+    'V' : 'Previsão de pagamento',
     'P' : 'Pacialmente',
     'C' : 'Conta a pagar'
 }
 
+
+export const ITCON_PAG_STATUS_RECEBER = {
+    'Q' : 'Quitado',
+    'V' : 'Vencido',
+    'P' : 'Pacialmente',
+    'C' : 'Conta a receber'
+}
+export const ITCONREC_REC_STATUS_CLASS_MAP = {
+    'Q' : 'bg-gray-500',
+    'V' : 'bg-red-500',
+    'P' : 'bg-orange-400',
+    'C' : 'bg-emerald-900'
+}
 export const ITCON_PAG_STATUS_CLASS_MAP = {
     'Q' : 'bg-green-500',
-    'V' : 'bg-amber-500',
+    'V' : 'bg-indigo-900',
     'P' : 'bg-orange-400',
-    'C' : 'bg-sky-400'
+    'C' : 'bg-red-700'
 }
 
 export const CONPAG_VENCIMENTO_TEXT = {
     'P' : 'text-green-400',
     'V' : 'text-red-300'
+}
+
+export const ITCON_REC_TIPO_CONTA_STATUS = {
+     'A' : 'ADM CARTAO CREDITO',
+     'C' : 'CONVENIO',
+     'D' : 'DIVERSOS',
+     'F' : 'FUNCIONARIO',
+     'M' : 'MENSALIDADE',
+     'P' : 'CONTA PACIENTE',
+     'R' : 'FARMACIA'
+}
+
+export const ITCON_REC_TIPO_CONTA_CLASS = {
+    'A': 'bg-blue-500',
+    'C': 'bg-sky-400',
+    'D': 'bg-fuchsia-600',
+    'F': 'bg-teal-500',
+    'M': 'bg-emerald-300',
+    'P': 'bg-indigo-900',
+    'R': 'bg-red-400'
 }
